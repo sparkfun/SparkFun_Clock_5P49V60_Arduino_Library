d7a26a345ae77a56a9747ceee446e27ca6b4b6fd
Elias Santistevan <elias.santistevan@sparkfun.local>
2019-09-18 15:53:02 -0600

    Adds functions for adding additional capacitance to the XIN and XOUT lines on the crystal: addCrystalLoadCap and readCrystalCapVal, as well as a function for persistent clock signals in shutdown.

--- src/SparkFun_5P49V60.cpp
+++ src/SparkFun_5P49V60.cpp
@@ -1,8 +1,8 @@
 /*
   This is a library for...
   By: Elias Santistevan
-  Date: 
-  License: This code is public domain but you buy me a beer if you use this and 
+  Date:
+  License: This code is public domain but you buy me a beer if you use this and
   we meet someday (Beerware license).
 
   Feel like supporting our work? Buy a board from SparkFun!
@@ -14,16 +14,16 @@ SparkFun_Clock::Class_Name(uint8_t address){  _address = address; } //Constructo
 
 bool SparkFun_Clock::begin( TwoWire &wirePort )
 {
-  
+
   _i2cPort = &wirePort;
 
   _i2cPort->beginTransmission(_address);
   delay(100); // 100ms Startup time
   uint8_t _ret = _i2cPort->endTransmission();
   if( !_ret )
-    return true; 
-  else 
-    return false; 
+    return true;
+  else
+    return false;
 
 }
 
@@ -32,8 +32,8 @@ void SparkFun_Clock:changeI2CAddress(uint8_t addr_selec){
 
   if (addr_selec != 0 || addr_selec != 1)
     return;
-  
-  _writeRegister(OTP_CONTROL_REG, OTP_ADDR_MASK, addr_selec, POS_ZERO);
+
+  _writeRegister(OTP_CONTROL_REG, MASK_ONE, addr_selec, POS_ZERO);
 
 }
 
@@ -41,27 +41,118 @@ void SparkFun_Clock:changeI2CAddress(uint8_t addr_selec){
 uint8_t SparkFun_Clock:readI2CAddress(){
 
   uint8_t regVal = _readRegister(OTP_CONTROL_REG);
-  regVal &= ~OTP_ADDR_MASK;
+  regVal &= ~MASK_ONE;
   return regVal;
 
 }
 
-
-//
-void SparkFun_Clock::enableShutDown(uint8_t sd_val){
-}
-
 //
 void SparkFun_Clock::sdActiveState(uint8_t state){
-  
+
   if (state != HIGH || state != LOW)
     return;
 
-  _writeRegister(SHUTDOWN_REG, MASK_ONE, state, POS_ONE) 
+  _writeRegister(SHUTDOWN_REG, MASK_ONE, state, POS_ONE)
+
+}
+
+// This function allows the given clock line to remain on when the SparkFun
+// Clock Generator is put into shutdown mode.
+void SparkFun_Clock::persEnableClock(uint8_t clock){
+
+  uint8_t _bit_pos;
+  uint8_t _mask;
+
+  if (clock < 0 || clock > 4)
+    return;
+
+  if (clock == 0) {
+    _bit_pos = POS_SEVEN;
+    _mask = MASK_EIGHT_MSB;
+  }
+  else if (clock == 1){
+    _bit_pos = POS_SIX;
+    _mask = MASK_FOUR_MSB;
+  }
+  else if (clock == 2) {
+    _bit_pos = POS_FIVE;
+    _mask = MASK_TWO_MSB;
+  }
+  else if (clock == 3){
+    _bit_pos = POS_FOUR;
+    _mask = MASK_ONE_MSB;
+  }
+  else (clock == 4){
+    _bit_pos = POS_THREE;
+    _mask = MASK_FOUR;
+  }
+
+  _writeRegister(CLK_OE_FUNC_REG, _mask, clock, _bit_pos);
+}
+
+
+// This function adds the available internal capacitors to the given pin on the
+// crystal. The two available pins can be taken directly from the datasheet, or
+// schematic: XIN and XOUT.
+void SparkFun_Clock::addCrystalLoadCap(uint8_t pin, float capVal){
+
+  uint8_t _register;
+  uint8_t _bit_pos;
+  uint8_t _mask;
+
+  if (pin == XIN)
+    _register = LOAD_CAP_REG_ONE;
+  else if (pin == XOUT)
+    _register = LOAD_CAP_REG_TWO;
+  else return;
+
+  if (capVal == 1){
+    _bit_pos = POS_TWO;
+    _mask = MASK_FOUR;
+  }
+  else if (capVal == 2){
+    _bit_pos = POS_THREE;
+    _mask = MASK_EIGHT;
+  }
+  else if (capVal == 3){
+    _bit_pos = POS_FOUR;
+    _mask = MASK_ONE_MSB;
+  }
+  else if (capVal == 4){
+    _bit_pos = POS_FIVE;
+    _mask = MASK_TWO_MSB;
+  }
+  else if (capVal == 5){
+    _bit_pos = POS_SIX;
+    _mask = MASK_FOUR_MSB;
+  }
+  else if (capVal == 6){
+    _bit_pos = POS_SEVEN;
+    _mask = MASK_EIGHT_MSB;
+  }
+  else return;
+
+  _writeRegister(_register, _mask, capVal, _bit_pos);
+
+}
+
+//
+float SparkFun_Clock::readCrystalCapVal(uint8_t pin){
+
+  uint8_t _register;
+
+  if (pin == XIN)
+    _register = LOAD_CAP_REG_ONE;
+  else if (pin == XOUT)
+    _register = LOAD_CAP_REG_TWO;
+  else return;
+
+  uint8_t regVal = _readRegister(_register);
+  regVal &= MASK_THREE;
 
 }
 // This generic function handles I2C write commands for modifying individual
-// bits in an eight bit register. Paramaters include the register's address, a mask 
+// bits in an eight bit register. Paramaters include the register's address, a mask
 // for bits that are ignored, the bits to write, and the bits' starting
 // position.
 void SparkFun_Clock::_writeRegister(uint8_t _wReg, uint8_t _mask, uint8_t _bits, uint8_t _startPosition)
@@ -76,13 +167,13 @@ void SparkFun_Clock::_writeRegister(uint8_t _wReg, uint8_t _mask, uint8_t _bits,
 }
 
 // This generic function reads an eight bit register. It takes the register's
-// address as its' parameter. 
+// address as its' parameter.
 uint8_t SparkFun_Clock::_readRegister(uint8_t _reg)
 {
 
   _i2cPort->write(_reg); // Moves pointer to register.
   _i2cPort->endTransmission(false); // 'False' here sends a restart message so that bus is not released
-  _i2cPort->requestFrom(_address, 1); // Read the register, only ever once. 
+  _i2cPort->requestFrom(_address, 1); // Read the register, only ever once.
   _regValue = _i2cPort->read();
   return(_regValue);
 
--- src/SparkFun_5P49V60.h
+++ src/SparkFun_5P49V60.h
@@ -4,116 +4,119 @@
 #include <Wire.h>
 #include <Arduino.h>
 
-// 7 bit unshifted addresses: 
-#define DEF_ADDR 0xD4 
+// 7 bit unshifted addresses:
+#define DEF_ADDR 0xD4
 #define ALT_ADDR 0xD0
 
-#define ALT     0x01
-#define DEF     0x00
-#define LOW     0x00
-#define HIGH    0x01
-#define DISABLE 0x00
-#define ENABLE  0x01
+#define  ALT     0x01
+#define  DEF     0x00
+#define  LOW     0x00
+#define  HIGH    0x01
+#define  DISABLE 0x00
+#define  ENABLE  0x01
+#define  XIN     0x01
+#define  XOUT    0x02
 
 enum REGISTER_INDEX {
-  
+
   OTP_CONTROL_REG            = 0x00,
   SHUTDOWN_REG               = 0x10,
-  VCO_BAND_REG               = 0x11, 
-  LOAD_CAP_REG               = 0x12,
+  VCO_BAND_REG               = 0x11,
+  LOAD_CAP_REG_ONE           = 0x12,
+  LOAD_CAP_REG_TWO           = 0x12,
   REF_DIVIDER_REG            = 0x15,
-  DIVIDER_VCO_REG            = 0x16, 
+  DIVIDER_VCO_REG            = 0x16,
 
-  FDB_INT_DIV_REG_ONE        = 0x17, 
-  FDB_INT_DIV_REG_TWO        = 0x18, 
+  FDB_INT_DIV_REG_ONE        = 0x17,
+  FDB_INT_DIV_REG_TWO        = 0x18,
 
-  FDB_FRAC_DIV_REG_ONE       = 0x19, 
-  FDB_FRAC_DIV_REG_TWO       = 0x1A, 
-  FDB_FRAC_DIV_REG_THR       = 0x1B, 
+  FDB_FRAC_DIV_REG_ONE       = 0x19,
+  FDB_FRAC_DIV_REG_TWO       = 0x1A,
+  FDB_FRAC_DIV_REG_THR       = 0x1B,
 
-  RC_CONTR_REG_ONE           = 0x1B, 
-  RC_CONTR_REG_TWO           = 0x1C, 
+  RC_CONTR_REG_ONE           = 0x1B,
+  RC_CONTR_REG_TWO           = 0x1C,
 
-  OUT_IDIV_REG_ONE           = 0x2D, 
-  OUT_IDIV_REG_TWO           = 0x2E, 
+  OUT_IDIV_REG_ONE           = 0x2D,
+  OUT_IDIV_REG_TWO           = 0x2E,
 
-  OUT_IDIV_TWO_REG_ONE       = 0x3D, 
-  OUT_IDIV_TWO_REG_TWO       = 0x3E, 
+  OUT_IDIV_TWO_REG_ONE       = 0x3D,
+  OUT_IDIV_TWO_REG_TWO       = 0x3E,
 
-  OUT_IDIV_THR_REG_ONE       = 0x4D, 
-  OUT_IDIV_THR_REG_TWO       = 0x4E, 
+  OUT_IDIV_THR_REG_ONE       = 0x4D,
+  OUT_IDIV_THR_REG_TWO       = 0x4E,
 
-  OUT_IDIV_FOUR_REG_ONE      = 0x5D, 
-  OUT_IDIV_FOUR_REG_TWO      = 0x5E, 
+  OUT_IDIV_FOUR_REG_ONE      = 0x5D,
+  OUT_IDIV_FOUR_REG_TWO      = 0x5E,
 
-  OUT_FDIV_REG_ONE           = 0x22, 
-  OUT_FDIV_REG_TWO           = 0x23, 
-  OUT_FDIV_REG_THR           = 0x24, 
-  OUT_FDIV_REG_FOUR          = 0x25, 
+  OUT_FDIV_REG_ONE           = 0x22,
+  OUT_FDIV_REG_TWO           = 0x23,
+  OUT_FDIV_REG_THR           = 0x24,
+  OUT_FDIV_REG_FOUR          = 0x25,
 
-  OUT_STEP_SPRD_REG_ONE      = 0x26, 
-  OUT_STEP_SPRD_REG_TWO      = 0x27, 
-  OUT_STEP_SPRD_REG_THR      = 0x28, 
+  OUT_STEP_SPRD_REG_ONE      = 0x26,
+  OUT_STEP_SPRD_REG_TWO      = 0x27,
+  OUT_STEP_SPRD_REG_THR      = 0x28,
 
-  OUT_SPRD_RATE_REG_ONE      = 0x29, 
-  OUT_SPRD_RATE_REG_TWO      = 0x2A, 
+  OUT_SPRD_RATE_REG_ONE      = 0x29,
+  OUT_SPRD_RATE_REG_TWO      = 0x2A,
 
-  OUT_FDIV_TWO_REG_ONE       = 0x32, 
-  OUT_FDIV_TWO_REG_TWO       = 0x33, 
-  OUT_FDIV_TWO_REG_THR       = 0x34, 
-  OUT_FDIV_TWO_REG_FOUR      = 0x35, 
+  OUT_FDIV_TWO_REG_ONE       = 0x32,
+  OUT_FDIV_TWO_REG_TWO       = 0x33,
+  OUT_FDIV_TWO_REG_THR       = 0x34,
+  OUT_FDIV_TWO_REG_FOUR      = 0x35,
 
-  OUT_STEP_SPRD_TWO_REG_ONE  = 0x36, 
-  OUT_STEP_SPRD_TWO_REG_TWO  = 0x37, 
-  OUT_STEP_SPRD_TWO_REG_THR  = 0x38, 
+  OUT_STEP_SPRD_TWO_REG_ONE  = 0x36,
+  OUT_STEP_SPRD_TWO_REG_TWO  = 0x37,
+  OUT_STEP_SPRD_TWO_REG_THR  = 0x38,
 
-  OUT_SPRD_RATE_TWO_REG_ONE  = 0x39, 
-  OUT_SPRD_RATE_TWO_REG_TWO  = 0x3A, 
+  OUT_SPRD_RATE_TWO_REG_ONE  = 0x39,
+  OUT_SPRD_RATE_TWO_REG_TWO  = 0x3A,
 
-  OUT_FDIV_THR_REG_ONE       = 0x42, 
-  OUT_FDIV_THR_REG_TWO       = 0x43, 
-  OUT_FDIV_THR_REG_THR       = 0x44, 
-  OUT_FDIV_THR_REG_FOUR      = 0x45, 
+  OUT_FDIV_THR_REG_ONE       = 0x42,
+  OUT_FDIV_THR_REG_TWO       = 0x43,
+  OUT_FDIV_THR_REG_THR       = 0x44,
+  OUT_FDIV_THR_REG_FOUR      = 0x45,
 
-  OUT_STEP_SPRD_THR_REG_ONE  = 0x46, 
-  OUT_STEP_SPRD_THR_REG_TWO  = 0x47, 
-  OUT_STEP_SPRD_THR_REG_THR  = 0x48, 
+  OUT_STEP_SPRD_THR_REG_ONE  = 0x46,
+  OUT_STEP_SPRD_THR_REG_TWO  = 0x47,
+  OUT_STEP_SPRD_THR_REG_THR  = 0x48,
 
-  OUT_SPRD_RATE_THR_REG_ONE  = 0x49, 
-  OUT_SPRD_RATE_THR_REG_TWO  = 0x4A, 
+  OUT_SPRD_RATE_THR_REG_ONE  = 0x49,
+  OUT_SPRD_RATE_THR_REG_TWO  = 0x4A,
 
-  OUT_FDIV_FOUR_REG_ONE      = 0x42, 
-  OUT_FDIV_FOUR_REG_TWO      = 0x43, 
-  OUT_FDIV_FOUR_REG_THR      = 0x44, 
-  OUT_FDIV_FOUR_REG_FOUR     = 0x45, 
+  OUT_FDIV_FOUR_REG_ONE      = 0x42,
+  OUT_FDIV_FOUR_REG_TWO      = 0x43,
+  OUT_FDIV_FOUR_REG_THR      = 0x44,
+  OUT_FDIV_FOUR_REG_FOUR     = 0x45,
 
-  OUT_STEP_SPRD_FOUR_REG_ONE = 0x46, 
-  OUT_STEP_SPRD_FOUR_REG_TWO = 0x47, 
-  OUT_STEP_SPRD_FOUR_REG_THR = 0x48, 
+  OUT_STEP_SPRD_FOUR_REG_ONE = 0x46,
+  OUT_STEP_SPRD_FOUR_REG_TWO = 0x47,
+  OUT_STEP_SPRD_FOUR_REG_THR = 0x48,
 
-  OUT_SPRD_RATE_FOUR_REG_ONE = 0x49, 
-  OUT_SPRD_RATE_FOUR_REG_TWO = 0x4A, 
-  
-  OUT_ISKEW_REG_ONE          = 0x2B, 
-  OUT_ISKEW_REG_TWO          = 0x2C, 
+  OUT_SPRD_RATE_FOUR_REG_ONE = 0x49,
+  OUT_SPRD_RATE_FOUR_REG_TWO = 0x4A,
 
-  OUT_FSKEW_REG              = 0x2F, 
+  OUT_ISKEW_REG_ONE          = 0x2B,
+  OUT_ISKEW_REG_TWO          = 0x2C,
 
-  OUT_ISKEW_TWO_REG_ONE      = 0x3B, 
-  OUT_ISKEW_TWO_REG_TWO      = 0x3C, 
+  OUT_FSKEW_REG              = 0x2F,
 
-  OUT_FSKEW_TWO_REG          = 0x3F, 
+  OUT_ISKEW_TWO_REG_ONE      = 0x3B,
+  OUT_ISKEW_TWO_REG_TWO      = 0x3C,
 
-  OUT_ISKEW_THR_REG_ONE      = 0x4B, 
-  OUT_ISKEW_THR_REG_TWO      = 0x4C, 
+  OUT_FSKEW_TWO_REG          = 0x3F,
 
-  OUT_FSKEW_THR_REG          = 0x4F, 
+  OUT_ISKEW_THR_REG_ONE      = 0x4B,
+  OUT_ISKEW_THR_REG_TWO      = 0x4C,
 
-  OUT_ISKEW_FOUR_REG_ONE     = 0x5B, 
-  OUT_ISKEW_FOUR_REG_TWO     = 0x5C, 
+  OUT_FSKEW_THR_REG          = 0x4F,
 
-  OUT_FSKEW_FOUR_REG         = 0x5F, 
-  
+  OUT_ISKEW_FOUR_REG_ONE     = 0x5B,
+  OUT_ISKEW_FOUR_REG_TWO     = 0x5C,
+
+  OUT_FSKEW_FOUR_REG         = 0x5F,
+
   CLK_ONE_OUT_CNFIG_REG_ONE  = 0x60,
   CLK_ONE_OUT_CNFIG_REG_TWO  = 0x61,
 
@@ -125,72 +128,75 @@ enum REGISTER_INDEX {
 
   CLK_FOUR_OUT_CNFIG_REG_ONE = 0x66,
   CLK_FOUR_OUT_CNFIG_REG_TWO = 0x67,
-  
+
   CLK_OE_FUNC_REG            = 0x68,
   CLK_OS_FUNC_REG            = 0x69
 
 };
 
 enum MASK_INDEX {
-  
-  OTP_ADDR_MASK  = 0xFE,
-  GLOB_SHUT_MASK = 0xFE,
 
-  MASK_ONE       = 0xFE, // yes
-  MASK_TWO       = 0xFD, // yes
-  MASK_THREE     = 0xF3,
-  MASK_FOUR      = 0xF4,
-  MASK_FIVE      = 0xF5,
-  MASK_SIX       = 0xF6,
-  MASK_SEVEN     = 0xF7,
-  MASK_EIGHT     = 0xF8,
-  MASK_NINE      = 0xF9,
-  MASK_TEN       = 0xFA,
-  MASK_ELEVEN    = 0xFB,
-  MASK_TWELVE    = 0xFC,
-  MASK_THIRT     = 0xFD,
-  MASK_FOURT     = 0xFE,
-  MASK_FIFT      = 0xFF
+  // LSB
+  MASK_ONE    = 0xFE, // yes
+  MASK_TWO    = 0xFD, // yes
+  MASK_THREE  = 0xFC,
+  MASK_FOUR   = 0xFB,
+  MASK_FIVE   = 0xFA,
+  MASK_SIX    = 0xF9,
+  MASK_SEVEN  = 0xF8,
+  MASK_EIGHT  = 0xF7,
+  MASK_NINE   = 0xF6,
+  MASK_TEN    = 0xF5,
+  MASK_ELEVEN = 0xF4,
+  MASK_TWELVE = 0xF3,
+  MASK_THIRT  = 0xF2,
+  MASK_FOURT  = 0xF1,
+  MASK_FIFT   = 0xF0,
+
+  MASK_EIGHT_MSB = 0x7F,
+  MASK_FOUR_MSB = 0xBF,
+  MASK_TWO_MSB = 0xDF,
+  MASK_ONE_MSB = 0xEF
 
 };
-  
+
 enum BIT_POS_INDEX {
-  
-  POS_ZERO = 0x00, 
-  POS_ONE, 
-  POS_TWO, 
-  POS_THREE, 
-  POS_FOUR, 
-  POS_FIVE, 
-  POS_SIX, 
-  POS_SEVEN 
+
+  POS_ZERO = 0x00,
+  POS_ONE,
+  POS_TWO,
+  POS_THREE,
+  POS_FOUR,
+  POS_FIVE,
+  POS_SIX,
+  POS_SEVEN
 
 };
 
 class SparkFun_Clock
-{  
+{
   public:
-    
+
     // Public Variables
-    
+
     //Function declarations
     SparkFun_Clock(uint8_t address); // I2C Constructor
 
     bool begin(TwoWire &wirePort = Wire); // begin function
 
   private:
-    
+
     // Private Variables
     uint8_t _address;
 
     // This generic function handles I2C write commands for modifying individual
-    // bits in an eight bit register. Paramaters include the register's address, a mask 
+    // bits in an eight bit register. Paramaters include the register's address, a mask
     // for bits that are ignored, the bits to write, and the bits' starting
     // position.
     void writeRegister(uint8_t, uint8_t, uint8_t, uint8_t);
 
     // This generic function reads an eight bit register. It takes the register's
-    // address as its' parameter. 
+    // address as its' parameter.
     uint8_t readRegister(uint8_t);
 
     TwoWire *_i2cPort;
